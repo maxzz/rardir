71c39d3301ef245c54f2e640d21f58eaf8572b87
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-07 18:48:46 -0700

    WIP. handleFolder. split createdRarFile monolith

--- src/app/2-created-rar-file.ts
+++ src/app/2-created-rar-file.ts
@@ -4,12 +4,10 @@ import { notes } from "../app-utils/app-notes.js";
 import { fnames } from "../app-utils/utils-app.js";
 import { AppUtils } from "../app-utils/utils-dir.js";
 
-export type FItem = OsStuff.FileItem & { ext: fnames.extType; };
+type FItem = OsStuff.FileItem & { ext: fnames.extType; };
 
-export function createdRarFile(targetFolder: string, filesAndFolders: OsStuff.FolderItem): true | undefined {
-
-    // 3. Get what we have now inside this folder.
-    let fItems: FItem[] = filesAndFolders.files.map(
+function addExtentionTypes(files: OsStuff.FileItem[]): FItem[] {
+    const rv: FItem[] = files.map(
         (fileItem: OsStuff.FileItem) => {
             return {
                 ...fileItem,
@@ -17,12 +15,45 @@ export function createdRarFile(targetFolder: string, filesAndFolders: OsStuff.Fo
             };
         }
     );
+    return rv;
+}
 
-    // 4.1. Check for combination: .url + [.mht] + .torrent + !tm.rar + ![<media files>] // mht is optional
+type FileGroups = {
+    tors: FItem[];
+    urls: FItem[];
+    mhts: FItem[];
+    txts: FItem[];
+};
+
+function groupByExt(files:  OsStuff.FileItem[]): FileGroups {
+    // 0. Get what we have now inside this folder.
+
+    const fItems: FItem[] = addExtentionTypes(files);
+
     let tors: FItem[] = fItems.filter((fitem: FItem) => fitem.ext === fnames.extType.tor);
     let urls: FItem[] = fItems.filter((fitem: FItem) => fitem.ext === fnames.extType.url);
     let mhts: FItem[] = fItems.filter((fitem: FItem) => fitem.ext === fnames.extType.mht);
     let txts: FItem[] = fItems.filter((fitem: FItem) => fitem.ext === fnames.extType.txt);
+
+    return { tors, urls, mhts, txts };
+}
+
+function prepareShortFilenamesToRar(fileGroups: FileGroups): string[] {
+    const { tors, urls, mhts, txts } = fileGroups;
+
+    let smallFiles = [...tors, ...urls, ...mhts, ...txts].filter( // Filter out files more than 5MB (some mht are > 5MB)
+        (fitem: FItem) => fitem.size < 5000000
+    );
+
+    let filesToRar: string[] = smallFiles.map((fitem) => fitem.short);
+
+    return filesToRar;
+}
+
+function isOurFolder(fileGroups: FileGroups, targetFolder: string): true | undefined {
+    // 0. Check for combination: .url + [.mht] + .torrent + !tm.rar + ![<media files>] // mht is optional
+
+    const { tors, urls, mhts, txts } = fileGroups;
 
     let ourFolder = tors.length && urls.length || mhts.length && urls.length;
     if (!ourFolder) {
@@ -31,19 +62,27 @@ export function createdRarFile(targetFolder: string, filesAndFolders: OsStuff.Fo
     }
 
     notes.addProcessed(`    ${targetFolder}`);
+    return true; // as continue
+}
+
+export function createdRarFile(targetFolder: string, filesAndFolders: OsStuff.FolderItem): true | undefined {
+
+    let groups = groupByExt(filesAndFolders.files);
+
+    if (!isOurFolder(groups, targetFolder)) {
+        return;
+    }
+
+    const shortFnamesToRar = prepareShortFilenamesToRar(groups);
+    shortFnamesToRar.push(AppUtils.fnameDirsTxt);
 
     // 4.2. Move to .rar top level files.
     let rootDir2Rar = filesAndFolders.name;
     let fullNameRar = path.join(rootDir2Rar, 'tm.rar');
 
-    let smallFiles = [...tors, ...urls, ...mhts, ...txts].filter((fitem: FItem) => fitem.size < 5000000); // Filter out files more than 5MB (some mht are > 5MB)
-    let filesToRar: string[] = smallFiles.map((fitem) => fitem.short);
-
     // 4.3. Create dirs.txt and add to tm.rar.
     AppUtils.execCmdDir(targetFolder);
-    filesToRar.push(AppUtils.fnameDirsTxt);
-
-    AppUtils.createRarFile(fullNameRar, rootDir2Rar, filesToRar);
+    AppUtils.createRarFile(fullNameRar, rootDir2Rar, shortFnamesToRar);
 
     return true; // as continue
 }
