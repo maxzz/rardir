a250a4175f774fbf64cda23ebfd9249dce379314
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-03 14:21:39 -0700

    WIP. 2025-update. tsconfig.json failed. 6. add tsx debug config

--- src/8-app-utils/app-help.ts
+++ src/8-app-utils/app-help.ts
@@ -1,12 +1,12 @@
 import chalk from "node-chalk";
 //import { exitProcess } from "../utils/utils-errors";
 
-//import cfg from "../../package.json" assert { type: 'json' };
+import cfg from "../../package.json" assert { type: 'json' };
 
 //let cfg = require('../package.json');
 
-import fs from 'fs';
-const cfg = JSON.parse(fs.readFileSync(new URL('../../package.json', import.meta.url), 'utf8'));
+// import fs from 'fs';
+// const cfg = JSON.parse(fs.readFileSync(new URL('../../package.json', import.meta.url), 'utf8'));
 
 export const { name: programName } = cfg;
 
